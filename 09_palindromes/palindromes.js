5f031bcf9ad4743231b7d077119d2fffec49d21b
fgh319 <fengguihang@outlook.com>
2024-11-06 14:56:28 +0800

    完成09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const allowedChar = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const cleanStr = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => allowedChar.includes(char))
+    .join("");
+
+  const reverseStr = cleanStr.split("").reverse().join("");
+  return cleanStr === reverseStr;
 };
 
 // Do not edit below this line
